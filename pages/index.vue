20cba759a558974929fc909cb7ff16611a7d1660
Jean Carlos Ayala <ayalajeancarlos.91@gmail.com>
2020-08-20 22:54:34 +0100

    input binding

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="title">
-        El TODO de Gabri
-      </h1>
+      <h1 class="title">El TODO de {{ newTask }}</h1>
+    </div>
+    <div>
+      <form>
+        <input
+          v-model="newTask"
+          type="text"
+          class="border bg-green p-4 bg-green-500"
+        />
+        <button></button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      newTask: '',
+    }
+  },
+}
 </script>
